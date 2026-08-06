41d53d38bb201ba8b6ccac17bc44c0e5687383f8
Md. Ahammad Ali <146563026+ahmedstore75@users.noreply.github.com>
2026-08-07 04:05:59 +0600

    Replace IPTV URL with a new proxy URL

--- merge.py
+++ merge.py
@@ -5,7 +5,7 @@ import re
 urls = [
     "https://raw.githubusercontent.com/ahmedstore75/Iptvbdlive/main/mixiptvchannel.m3u",
     "https://raw.githubusercontent.com/ahan443/FAST-IPTV/refs/heads/main/combined_playlist.m3u",
-    "https://iptvlive-beta.vercel.app",
+    "https://iptv-proxy.ahmed-bd-org.workers.dev",
     "https://raw.githubusercontent.com/sm-monirulislam/SM-Live-TV/refs/heads/main/Combined_Live_TV.m3u"
 ]
 
